9140a8f6cfd2933fcc219a6ede48eb94c769ccd9
VasikaranR <vasichan2000@gmail.com>
2022-05-05 17:04:46 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibraryDashComponent } from './library-dash/library-dash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibraryDashComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/library-dash/library-dash.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-library-dash',
+  templateUrl: './library-dash.component.html',
+  styleUrls: ['./library-dash.component.scss']
+})
+export class LibraryDashComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
