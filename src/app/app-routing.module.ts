bc41583a9cfe91986ce9aed2714d8c3c385149e4
VASIKARAN R <61900574+VasikaranR@users.noreply.github.com>
2022-05-09 09:33:13 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+ {path:'',redirectTo:'login',pathMatch:'full'},
+ {path:'login',component:LoginComponent},
+ {path:'dashboard',component:LibraryDashComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
